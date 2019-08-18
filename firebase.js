9bbcd651cda7bd2c5b17efd26a8d46edb5252aec
yamaki01 <hiro.y.7322@docomo.ne.jp>
2019-08-18 15:00:50 +0900

    Add my excellent code`

--- firebase.js
+++ firebase.js
@@ -1,10 +1,4 @@
 firebase.initializeApp({
-<!-- TODO: Add SDKs for Firebase products that you want to use
-     https://firebase.google.com/docs/web/setup#config-web-app -->
-
-<script>
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
     apiKey: "AIzaSyBw71iaoXCwc10E3R3Zc4KszEte96sgn6U",
     authDomain: "test01-a296e.firebaseapp.com",
     databaseURL: "https://test01-a296e.firebaseio.com",
@@ -12,10 +6,6 @@ firebase.initializeApp({
     storageBucket: "test01-a296e.appspot.com",
     messagingSenderId: "869682482726",
     appId: "1:869682482726:web:09529a3b0ddfc429"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-</script>
 });
 
 const db = firebase.firestore();
